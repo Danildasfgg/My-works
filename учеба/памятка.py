b4ffefe47ae583a15b53a8ee1aaa79a57b353332
Danildasfgg <danildasfgg@gmail.com>
2025-01-24 23:53:45 +0300

    Первый день учебы, котоый буду заливать в гит хаб

---
+++ учеба/памятка.py
@@ -0,0 +1,8 @@
+bool = True
+str = "Строка"
+int = 9
+float = 9.0
+print(type(bool))
+print(type(str))
+print(type(int))
+print(type(float))
---
+++ учеба/учеба1день.py
@@ -0,0 +1,72 @@
+#первое задание
+is_ready = True
+print(type(is_ready))
+
+#второе задание
+password = 980790
+old_password = "81k29"
+
+print(str(password)) == old_password
+
+print(type(password))
+print(type(old_password))
+
+#третье задание
+price = 9.99
+print(int(price))
+
+#четвертое задание
+weeks = 12
+print(float(weeks))
+
+#пятое задание
+member = True
+not_member = False
+value = int(member)
+second_value = int(not_member)
+
+print(value)
+print(second_value)
+
+#шестое задание
+member = "Sam"
+middle_name = " "
+foot_size = 8.5
+siblings = 0
+
+boolean_member = bool(member)
+boolean_middle_name = bool(middle_name)
+boolean_foot_size = bool(foot_size)
+boolean_siblings = bool(siblings)
+
+print(boolean_member)
+print(boolean_middle_name)
+print(boolean_foot_size)
+print(boolean_siblings)
+
+#сельмое задание
+pets = 3
+kids = 0 
+has_pets = bool(pets)
+has_kids = bool(kids)
+
+print(has_pets)
+print(has_kids)
+
+#восьмое задание
+age = 16
+print(age < 16)
+
+#девятое задание
+average_score = 4.7
+print(int(average_score))
+
+#десятое задание
+detail = "I like it"
+response = bool(detail)
+print(response)
+
+#одиннадцатое задание
+detail = "I like it"
+response = bool(detail)
+print(response)
