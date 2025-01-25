b1d8aac1ca719ad51114eff9450e2e0094a0f944
Danildasfgg <danildasfgg@gmail.com>
2025-01-25 19:29:28 +0300

    второй день учусь

---
+++ учеба/учеба2день.py
@@ -0,0 +1,44 @@
+#первое задание
+print(100 > 1)
+
+#второе задание
+print(11 >= 11)
+
+#третье задание
+score = 32
+min_score = 30
+pas = score >= min_score
+print(pas)
+
+#четвертое задание
+score = 11
+min_score = 10
+pass_grade = score >= min_score
+print(pass_grade)
+
+#пятое задание
+print(5 < 5)
+
+#шестое задание
+copy = "results.xls"
+submission = "sales.xls"
+print(copy != submission)
+
+#седьмое задание
+print("nina" != "nina")
+
+#восьмое задание
+print(type(False))
+
+#девятое задание
+result = 100 + 1 
+print(result)
+
+#десятое задание
+last_name = "clooney"
+
+#одиннадцатое задание
+lenght = 44.34
+
+#двенадцатое задание
+score = 2
